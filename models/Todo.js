d5e0f6c69402388a6e510093154a0e03bc83c1be
hixsav <hi.savis@gmail.com>
2018-10-01 19:42:49 +0100

    Updated model and route

--- models/Todo.js
+++ models/Todo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const UserSchema = new Schema({
+const TodoSchema = new Schema({
 	itemname: {
 		type: String,
 		required: 'What is it you want to do?'
--- routes/api/v1/todos.js
+++ routes/api/v1/todos.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// Load Todo model
+const Todo = require('../../../models/Todo');
+
 // @route   GET api/v1/todos/test
 // @desc    Tests todos route
-// @access   Public
+// @access  Public
 apiRouter.get('/test', (req, res) => res.json({ message: 'Todos does work!' }));
+
+// @route   GET api/v1/todos/all
+// @desc    Lists all todos route
+// @access  Public
+apiRouter.get('/all', (req, res) => {
+	Todo.find()
+		.then((todos) => {
+			res.json(todos);
+		})
+		.catch((err) => res.status(404).json({ todo: 'There is nothing on your todos list' }));
+});
+
+// @route   POST api/v1/todo
+// @desc    Create or edit todos route
+// @access  Public
+apiRouter.post('/', (req, res) => {
+	Todo.create(req.body)
+		.then((newTodo) => {
+			res.status(201).json(newTodo);
+		})
+		.catch((err) => res.send(err));
+});
 
 module.exports = apiRouter;
